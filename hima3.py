9c6d7c9fb8113f486076cb37ee53384c4471264e
Tanuja Gurrapusala <tanujagurrapusala@gmail.com>
2026-09-15 10:28:45 +0530

    Add files via upload

---
+++ hima3.py
@@ -0,0 +1,6 @@
+l=int(input("enter the length of the rectangle"))
+b=int(input("enter the breadth of the rectangle"))
+area =l*b
+perimeter=2*a+b
+print("area is:",area)
+print("perimeter is:",perimeter)
